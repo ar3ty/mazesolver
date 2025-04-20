e2a38c10c862645bcc7282cd17eb6c59cfea9dc0
ar3ty <ar3tyrt5y@gmail.com>
2025-04-21 00:37:21 +0200

    add bfs, gui, customable grid

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from graphics import Window
 from maze import *
+from config import *
 
 def main():
-    win = Window(1000, 1000)
-
-    maze = Maze(50, 50, 10, 10, 50, 35, win)
-    is_solved = maze.solve()
-    if is_solved:
-        print("maze is solved")
-    else:
-        print("maze is not solved, smth is wrong")
-
+    
+    win = Window()
     win.wait_for_close()
+    
+    maze = Maze(win.input['Margin'],
+                win.input['Margin'], 
+                win.input['Number of columns'], 
+                win.input['Number of rows'], 
+                win.input['Cell width'], 
+                win.input['Cell height'],
+                win)
+    
+    win.wait_for_close()
+
+    ##is_solved = maze.solve()
+    ##if is_solved:
+        ##print("maze is solved")
+    ##else:
+        ##print("maze is not solved, smth is wrong")
+
+   
 
 if __name__=="__main__":
     main()
--- maze.py
+++ maze.py
@@ -121,19 +121,15 @@ class Maze:
             if y < j and x == i:
                 self._cells[i][j].has_top_wall = False
                 self._cells[x][y].has_bottom_wall = False
-                self._draw_cell(i,j)
             elif x > i and j == y:
                 self._cells[i][j].has_right_wall = False
                 self._cells[x][y].has_left_wall = False
-                self._draw_cell(i,j)
             elif y > j and x == i:
                 self._cells[i][j].has_bottom_wall = False
                 self._cells[x][y].has_top_wall = False
-                self._draw_cell(i,j)
             else:
                 self._cells[i][j].has_left_wall = False
                 self._cells[x][y].has_right_wall = False
-                self._draw_cell(i,j)
             self._break_walls_r(x,y)
 
     def _reset_cells_visited(self):
@@ -142,9 +138,14 @@ class Maze:
                 self._cells[i][j].visited = False
     
     def solve(self):
-        return self._solve_dfs(0,0)
-
-    def _solve_dfs(self, i, j):
+        l = Line(Point(self._x1 + self._cell_size_x/2, self._y1 + self._cell_size_y/2), Point(self._x1 + self._cell_size_x/2, self._y1 - self._cell_size_y/2))
+        self._win.draw_line(l, "red")
+        res = self._solve_bfs(0,0)
+        l = Line(Point(self._x1 + self._cell_size_x * (self._num_cols - 0.5), self._y1 + self._cell_size_y * (self._num_rows - 0.5)), Point(self._x1 + self._cell_size_x * (self._num_cols - 0.5), self._y1 + self._cell_size_y * (self._num_rows + 0.5)))
+        self._win.draw_line(l, "red")
+        return res
+    
+    def _solve_dfs_r(self, i, j):
         self._animate()
         self._cells[i][j].visited = True
         if i == self._num_cols - 1 and j == self._num_rows - 1:
@@ -152,39 +153,73 @@ class Maze:
         if j - 1 >= 0: 
             if self._cells[i][j-1].visited == False and self._cells[i][j].has_top_wall == False:
                 self._cells[i][j].draw_move(self._cells[i][j-1])
-                res = self._solve_dfs(i, j-1)
+                res = self._solve_dfs_r(i, j-1)
                 if res:
                     return True
                 self._cells[i][j].draw_move(self._cells[i][j-1], undo=True)
         if i + 1 < self._num_cols:  
             if self._cells[i+1][j].visited == False and self._cells[i][j].has_right_wall == False:
                 self._cells[i][j].draw_move(self._cells[i+1][j])
-                res = self._solve_dfs(i+1, j)
+                res = self._solve_dfs_r(i+1, j)
                 if res:
                     return True
                 self._cells[i][j].draw_move(self._cells[i+1][j], undo=True)
         if j + 1 < self._num_rows:  
             if self._cells[i][j+1].visited == False and self._cells[i][j].has_bottom_wall == False:
                 self._cells[i][j].draw_move(self._cells[i][j+1])
-                res = self._solve_dfs(i, j+1)
+                res = self._solve_dfs_r(i, j+1)
                 if res:
                     return True
                 self._cells[i][j].draw_move(self._cells[i][j+1], undo=True)
         if i - 1 >= 0: 
             if self._cells[i-1][j].visited == False and self._cells[i][j].has_left_wall == False:
                 self._cells[i][j].draw_move(self._cells[i-1][j])
-                res = self._solve_dfs(i-1, j)
+                res = self._solve_dfs_r(i-1, j)
                 if res:
                     return True
                 self._cells[i][j].draw_move(self._cells[i-1][j], undo=True)
         return False
-        
-
-
-
-            
-
-
-
-
-
+    
+    
+    def _solve_bfs(self, x, y):
+        self._animate()
+        to_visit = [(x,y)]
+        paths = {}
+        res = False
+        while len(to_visit) > 0:
+            i,j = to_visit.pop()
+            self._cells[i][j].visited = True
+            if i == self._num_cols - 1 and j == self._num_rows - 1:
+                res = True
+                break
+            if j - 1 >= 0: 
+                if self._cells[i][j-1].visited == False and self._cells[i][j].has_top_wall == False:
+                    self._cells[i][j].draw_move(self._cells[i][j-1], undo=True)
+                    self._animate()
+                    to_visit.insert(0,(i,j-1))
+                    paths[(i,j-1)] = (i,j)
+            if i + 1 < self._num_cols:  
+                if self._cells[i+1][j].visited == False and self._cells[i][j].has_right_wall == False:
+                    self._cells[i][j].draw_move(self._cells[i+1][j], undo=True)
+                    self._animate()
+                    to_visit.insert(0,(i+1,j))
+                    paths[(i+1,j)] = (i,j)
+            if j + 1 < self._num_rows:  
+                if self._cells[i][j+1].visited == False and self._cells[i][j].has_bottom_wall == False:
+                    self._cells[i][j].draw_move(self._cells[i][j+1], undo=True)
+                    self._animate()
+                    to_visit.insert(0,(i,j+1))
+                    paths[(i,j+1)] = (i,j)
+            if i - 1 >= 0: 
+                if self._cells[i-1][j].visited == False and self._cells[i][j].has_left_wall == False:
+                    self._cells[i][j].draw_move(self._cells[i-1][j], undo=True)
+                    self._animate()
+                    to_visit.insert(0,(i-1,j))
+                    paths[(i-1,j)] = (i,j)
+        if res == True:
+            current = (self._num_cols - 1, self._num_rows - 1)
+            while current != (0,0):
+                previous = paths[current]
+                self._cells[current[0]][current[1]].draw_move(self._cells[previous[0]][previous[1]])
+                current = previous
+        return res
